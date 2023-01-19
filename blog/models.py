a82bfb3dc0dd1443db67cc9879ff1f23245d8472
YeongMin Cho <zmzm3301@naver.com>
2023-01-19 10:34:04 +0900

    led 색상 db 저장

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    battery = models.CharField(max_length=10)
+    led_color = models.CharField(max_length=10)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.battery}, {self.led_color}'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        bat = request.POST['battery']
+        led_color = request.POST['led_color']
+
+        p = Post()
+
+        print("모델의 타입 : ", type(p))
+        print("모델의 형상 : ", p)
+
+        p.battery = bat
+        p.led_color = led_color
+
+        p.save()
+        return render(
+            request,
+            'blog/index.html',
+            {
+                'bat': bat,
+                'led_color': led_color,
+            }
+        )
+
+    if request.method == "GET":
+        posts = Post.objects.all()
+        return render(
+            request,
+            'blog/index.html',
+            {
+                "posts": posts
+            }
+        )
